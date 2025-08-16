72dce4a3f2e95bc54094566e908bf26e14bb44d5
unknown <raghavbhavyasingh@gmail.com>
2025-08-16 11:43:32 +0530

    created navbar and hero-section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Header from "./components/Header";
 
 function App() {
-  return <div></div>;
+  return (
+    <div className="w-full overflow-hidden">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
