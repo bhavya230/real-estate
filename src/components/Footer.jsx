70d4e0a34e6bc125996a1fa1198c0598ac51a1bb
unknown <raghavbhavyasingh@gmail.com>
2025-08-19 18:54:57 +0530

    footer added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { assets } from "../assets/assets";
+
+function Footer() {
+  return (
+    <div>
+      <div>
+        <div>
+          <img src={assets.logo_dark} alt="" />
+          <p>
+            {" "}
+            Lorem Ipsum is simply dummmy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text.
+          </p>
+        </div>
+        <div></div>
+        <div></div>
+      </div>
+      <div></div>
+    </div>
+  );
+}
+
+export default Footer;
