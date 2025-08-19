97c9c524ae4601c51bab3bc17aba03d026dafec9
unknown <raghavbhavyasingh@gmail.com>
2025-08-19 11:05:41 +0530

    working contact form using web3 forms

---
+++ src/components/Contact.jsx
@@ -0,0 +1,91 @@
+import React from "react";
+const access_key = import.meta.env.VITE_WEB3_API_KEY;
+export default function Contact() {
+  const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", access_key);
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("");
+      alert("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      alert(data.message);
+      setResult("");
+    }
+  };
+
+  return (
+    <div
+      className="text-center p-6 py-20 lg:px-32 w-full overflow-hidden"
+      id="Contact"
+    >
+      <h1 className="text-2xl sm:text-4xl font-bold mb-2 text-center">
+        {" "}
+        Contact{" "}
+        <span className="underline underline-offset-4 decoration-1 under font-light">
+          {" "}
+          With Us
+        </span>
+      </h1>
+      <p className="text-center text-gray-500 mb-12 max-w-80 mx-auto">
+        {" "}
+        Ready to Make a Move? Let's Build Your Future Together
+      </p>
+
+      <form
+        className="max-w-2xl mx-auto tetx-gray-600 pt-8 "
+        onSubmit={onSubmit}
+      >
+        <div className="flex flex-wrap">
+          <div className="w-full md:w-1/2 text-left">
+            Your Name
+            <input
+              className=" w-full border border-gray-300 rounded py-3 px-4 mt-2"
+              type="text"
+              placeholder="Your Name"
+              required
+              name="Name"
+            />
+          </div>
+          <div className="w-full md:w-1/2 text-left md:pl-4">
+            Your Email
+            <input
+              className=" w-full border border-gray-300 rounded py-3 px-4 mt-2"
+              type="email"
+              placeholder="Your Email"
+              required
+              name="Email"
+            />
+          </div>
+        </div>
+
+        <div className="my-6 text-left">
+          Message
+          <textarea
+            name="Message"
+            placeholder="Message"
+            required
+            className="w-full border border-gray-300 rounded py-3 px-4 h-48 resize-none"
+          ></textarea>
+        </div>
+        <button className="bg-blue-600 text-white py-2 px-12 mb-10 rounded cursor-pointer">
+          {result ? result : "Send Message"}
+        </button>
+      </form>
+    </div>
+  );
+}
